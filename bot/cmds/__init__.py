d19a808cc47de5830eaecfe33c55cf6deba07b90
Maxim Mosin <me@lznet.dev>
2022-05-12 03:34:43 +0900

    Save progress

--- bot/cmds/__init__.py
+++ bot/cmds/__init__.py
@@ -1,1 +1,2 @@
 from . import about
+from . import start
---
+++ bot/cmds/tracking.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+#  MajorExpressTrackingBot - Tracking cmds.
+#  Created by LulzLoL231 at 12/5/22
+#
+from aiogram import types
+
+from ..runtime import bot, log, majorapi, db
